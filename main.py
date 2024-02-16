d24e6027c638a48497107023bbee86ae4ff949bb
Deus <119631661+OldKokoroz@users.noreply.github.com>
2024-02-16 23:39:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ class TurningFunc:
     def __init__(self, translated="", as_text=""):
         self.translated = translated
         self.as_text = as_text
-        
+
     def mode1(self):
         # Text - Code Section
         while True:
             message = input("""
-        -------------------------------------
-        Message: """).lower()
+-------------------------------------
+Message: """).lower()
 
             if message == "99":
                 break
@@ -36,19 +36,19 @@ class TurningFunc:
                 sleep(0.05)
 
             print(f"""\n
-        Morse Code: {self.as_text}
-        -------------------------------------""")
+Morse Code: {self.as_text}
+-------------------------------------""")
 
             os.system(f"say your message is {self.translated}")
             self.as_text = ""
             self.translated = ""
-            
+
     def mode2(self):
         # Code - Text Section
         while True:
             code = input("""\n
-        -------------------------------------
-        Morse Code (Ex: ••••/–––/•) : """).split("/")
+-------------------------------------
+Morse Code (Ex: ••••/–––/•) : """).split("/")
 
             for _ in code:
                 if _ not in alphabet.keys():
@@ -62,45 +62,44 @@ class TurningFunc:
 
             os.system(f"say your message is {self.as_text}")
             print(f"""\n
-        Message: {self.as_text.capitalize()}
-        -------------------------------------
-        """)
+Message: {self.as_text.capitalize()}
+-------------------------------------
+""")
             self.as_text = ""
-    
+
     def for_cli(self):
         while True:
             try:
                 sleep(1)
-        
+
                 mode = input('''
-                |----------MODE-----------|
-                | 1 - Text -> Morse code  |
-                | 2 - Text <- Morse code  |
-                | 99 - Exit               |
-                |-------------------------|
-                |-> ''')
-                
+|----------MODE-----------|
+| 1 - Text -> Morse code  |
+| 2 - Text <- Morse code  |
+| 99 - Exit               |
+|-------------------------|
+|-> ''')
+
                 if mode == "1":
                     self.mode1()
-                    
+
                 elif mode == "2":
                     self.mode2()
-                    
+
                 elif mode == "99":
                     print("Exiting...")
                     exit(0)
-                    
+
                 else:
                     print("Invalid input!")
                     exit(1)
-            
+
             except ValueError or Exception:
                 print("Error!")
-        
+
             except KeyboardInterrupt:
                 print("Exiting")
                 exit()
 
 
-start = TurningFunc
-start.for_cli()
+TurningFunc.for_cli()
